6ccb5f1c0e582c431d5cdf2b236bac333c89e9ef
joselinnnnnn <157412207+joselinnnnnn@users.noreply.github.com>
2024-05-01 01:22:33 +0000

    ..

--- main.hpp
+++ main.hpp
@@ -1,3 +1,19 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+void writeFile(string);
+
+void readFile(string);
+
+void writeFile(string filename){
+    //
+}
+
+void readFile(string filename){
+    //
+}
+
