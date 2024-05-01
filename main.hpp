690e730f8164ae375bc265e11da2dd306808c54d
joselinnnnnn <157412207+joselinnnnnn@users.noreply.github.com>
2024-05-01 01:26:58 +0000

    write file done

--- main.hpp
+++ main.hpp
@@ -6,11 +6,42 @@ using namespace std;
 
 
 void writeFile(string);
-
 void readFile(string);
 
 void writeFile(string filename){
-    //
+      int empN;
+    ofstream ofs;
+    int IDs;
+    string name;
+    string dept;
+    double salary;
+
+    ofs.open("employee.txt");
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    cout << "Please enter the number of employees: ";
+    cin >> empN;
+  
+
+    for(int i = 0; i < empN; i++){
+        cin >> IDs;
+        cin >> name;
+        cin >> dept;
+        cin >> salary;
+          
+        ofs << IDs << " ";
+        ofs << name << " ";
+        ofs << dept << " ";
+        ofs << salary << " ";
+        ofs << endl << " ";
+    }
+
+    ofs.close();
+
 }
 
 void readFile(string filename){
